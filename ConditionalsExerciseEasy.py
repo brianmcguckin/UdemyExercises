1e609c22b22405c66f2372d6fe90a335708c3f36
Brian McGuckin <you@example.com>
2017-08-27 15:08:31 -0500

    initial commit

---
+++ ConditionalsExerciseEasy.py
@@ -0,0 +1,7 @@
+#write a program to check if a number is less than 10
+print("Enter a number: ")
+number = int(input())
+if number < 10:
+    print("Number is less than 10")
+else:
+    print("Number is greater than or equal to 10")
---
+++ ConditionalsExerciseMedium.py
@@ -0,0 +1,7 @@
+#write a program to check if a number is odd or even
+print("Enter a number: ")
+number = int(input())
+if number % 2 == 0:
+    print("Number is even")
+if number % 2 != 0:
+    print("Number is odd")
