5c2f6fcb151c769e205e7fa0758f5361059713b8
Brian McGuckin <you@example.com>
2017-08-29 13:11:06 -0500

    add 04StringsExercise.py

---
+++ 04StringsExercise.py
@@ -0,0 +1,17 @@
+#Easy
+#Write a program to create a string "Python programming is easy"
+string = "Python programming is easy"
+print(string)
+
+#Medium
+#Slice the first string and store "easy" in the second string
+string2 = string[22:26]
+print(string2)
+
+#Hard
+#Replace "easy" in the second string with " and powerful" and concatenate
+#both strings
+string2 = string2.replace("easy"," and powerful")
+string3 = string + string2
+print(string3
+)
