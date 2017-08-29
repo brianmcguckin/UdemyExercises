3a845c3efa9aff351ef76a7e450fd63cfe86075e
Brian McGuckin <you@example.com>
2017-08-29 13:03:05 -0500

    add 02NumbersExercise.py

---
+++ 02NumbersExercise.py
@@ -0,0 +1,10 @@
+#Easy
+#Write a program to assign the value 25 to an integer variable
+value = 25
+print(value)
+
+#Medium
+#Write a program to declare a floating value 25.77 and convert it to an integer
+value = 25.77
+value = int(value)
+print(value)
