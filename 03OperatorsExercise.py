9a0bdbc69e287d8de752e5f04d222345bec75c9e
Brian McGuckin <you@example.com>
2017-08-29 13:09:20 -0500

    Add 03OperatorsExercise.py

---
+++ 03OperatorsExercise.py
@@ -0,0 +1,16 @@
+#Easy
+#Write a program to add two numbers
+number0 = 30
+number1 = 15
+print(number0 + number1)
+
+#Medium
+#Solve manually: 2 + 3 * 4 = 14
+#Write a program to solve this equation
+print(2 + 3 * 4)
+#Did you get the same answer? Yes
+
+#Hard
+#Use a logical operator in between to comparison operators and
+#return the value as true
+print(5>3 or 6 == 2)
