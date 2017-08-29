2b5f63fd12021801297a2baca88e055676e3dff0
Brian McGuckin <you@example.com>
2017-08-29 13:11:47 -0500

    05ListExercise.py

---
+++ 05ListExercise.py
@@ -0,0 +1,15 @@
+#Easy
+#Write a program to create a list of 5 elements
+cubsPitchers = ["Arrieta","Lester","Hendricks","Lackey","Quintana"]
+print(cubsPitchers)
+
+#Medium
+#Update the value at the 3rd element of the list
+cubsPitchers[2] = "Montgomery"
+print(cubsPitchers)
+
+#Hard
+#Create another list of 3 elements. Concatenate the lists.
+cubsCatchers = ["Contreras","Avila","Rivera"]
+cubs = cubsPitchers + cubsCatchers
+print(cubs)
